89e057bf8cf101be67b73c39a7665ba7bfe14cab
JoseDaniel12 <josedaniel.alvaradof@gmail.com>
2023-02-21 14:02:31 -0600

    se agrego la clase 5

---
+++ Unidad 4/Clase5/src/main/java/ipc1/clase5/AppState.java
@@ -0,0 +1,11 @@
+package ipc1.clase5;
+
+import java.util.LinkedList;
+
+/**
+ *
+ * @author josed
+ */
+public class AppState {
+    static LinkedList<String> operaciones = new LinkedList<String>();
+}
---
+++ Unidad 4/Clase5/src/main/java/ipc1/clase5/Clase5.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ipc1.clase5;
+
+/**
+ *
+ * @author josed
+ */
+public class Clase5 {
+
+    public static void main(String[] args) {
+        LoginFrame loginFrame = new LoginFrame();
+        loginFrame.setVisible(true);
+    }
+}
